1d14fdb5db090a611d94da9bb6904c3fd1e34bae
Jennifer Luk <jenniferluk@Jennifers-MacBook-Air.local>
2022-11-05 14:36:44 -0400

    added navbar

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {AppBar, Typography} from '@mui/material';
+
+const Navbar = () => {
+    return (
+        <AppBar><Typography>Test</Typography></AppBar>
+
+    )
+}
+
+export default Navbar
