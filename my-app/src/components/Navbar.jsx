f9a63ee4db5bbe0bec5f1e21e3100af41508dcf0
Jennifer Luk <jenniferluk@Jennifers-MacBook-Air.local>
2022-11-05 15:13:57 -0400

    modified navbar features

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from "react"
-import {AppBar, Typography} from '@mui/material';
+import {AppBar, Typography, Toolbar, Tabs, Tab, Button} from '@mui/material';
 
 const Navbar = () => {
     return (
-        <AppBar><Typography>Test</Typography></AppBar>
+        <React.Fragment>
+            <AppBar sx={{background: '#2c82c9'}}>
+                <Toolbar>
+                    <Typography>HOUSING</Typography>
 
+                    <Tabs sx={{marginLeft: 'auto'}} textColor="inherit">
+                        <Tab label="Map Search"/>
+                        <Tab label="For Rent"/>
+                        <Tab label="Advertise"/>
+                    </Tabs>
+
+                    <Button variant="contained">Login{""}</Button>
+                    <Button sx={{marginLeft: '10px'}}variant="contained">Get Started{""}</Button>
+
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
     )
 }
 
