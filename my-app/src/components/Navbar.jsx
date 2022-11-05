24558e9f1984ef8b80a35b38d4ec2d885d9d748d
Jennifer Luk <jenniferluk@Jennifers-MacBook-Air.local>
2022-11-05 19:56:24 -0400

    create feature cards

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,23 +1,23 @@
 import React from "react"
 import './Navbar.css'
 import { Button, Toolbar } from "@mui/material";
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 const Navbar = () => {
     return (
         <div className="navBar container">
         <div className="navLogo">
-            Test
+            <a href="/">Test</a>
         </div>
 
         <div className="navLinks">
-            <a href="/">Map Search</a>
-            <a href="/">Rent</a>
-            <a href="/">Advertise</a>
+            <a href="/map">Map Search</a>
+            <a href="/for-rent">Rent</a>
+            <a href="/sell">Advertise</a>
         </div>
         <div>
            <Toolbar>
-                <Button sx={{marginLeft: 'auto', color:'white'}} variant="contained" color="warning">Login</Button>
-                <Button sx={{marginLeft: '10px', color:'white'}} variant="contained" color="warning">Sign Up</Button>
+                <Button sx={{marginLeft: 'auto', color:'white'}} variant="contained" color="warning" endIcon={<AccountCircle />}>Login</Button>
            </Toolbar>
         </div>
         </div>
