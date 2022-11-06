f1a1e45536ab9a7d192ff27efb4786c252777c17
Matthew <matthew_huynh09@yahoo.com>
2022-11-06 09:48:29 -0500

    changed markers

--- my-app/src/components/Locate.jsx
+++ my-app/src/components/Locate.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { Marker } from '@react-google-maps/api'
 import Navbar from './Navbar';
-import {featured} from './data/data';
+
 
 export class Locate extends Component {
   render() {
@@ -20,13 +20,12 @@ export class Locate extends Component {
           }
         }
       >
-        {featured.map((val, index) => {
-          const { lat, lng } = val
-          return(
-            <Marker key={index} position={{lat: {lat}, lng: {lng}}}></Marker>
-          )
-        })}
-        
+        <Marker position={{lat: 43.2562975, lng: -79.9201851}} 
+        ></Marker>
+        <Marker position={{lat: 43.2570352, lng: -79.9197441}} 
+        ></Marker>
+        <Marker position={{lat: 43.2584612, lng: -79.9154525}} 
+        ></Marker>
       </Map>
       </div>
     );
