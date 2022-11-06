9723ecc349ceed163a3c0a79ddb85ffbd06fafb0
Matthew <matthew_huynh09@yahoo.com>
2022-11-06 08:48:38 -0500

    add map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Card from './components/Card'
+import Locate from './components/Locate'
 
 function App() {
   return (
    <div className="App"> 
       <Card/>
+      <Locate></Locate>
    </div>
   );
 }
